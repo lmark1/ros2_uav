7ab285f9b8dc80be91b36bedc3f5846def0f5372
Lovro Markovic <lovro.markovic@fer.hr>
2022-02-04 15:25:44 +0100

    integrate ROS2 with existing simulation in ros1

---
+++ README.md
@@ -0,0 +1,7 @@
+# ROS2 UAV
+
+- Bridge all the UAV simulation topics to ROS2
+    - [x] mavros_msgs
+    - [x] geographic_msgs
+    - [ ] gazebo_msgs
+- [ ] Make this into a ROS2 package
--- install_rosbridge.sh
+++ install_rosbridge.sh
@@ -13,13 +13,42 @@ CATKIN_WS=/root/uav_ws
 # create ros2_workspace
 [ ! -e $ROS_BRIDGE_WS_PATH/src ] && mkdir -p $ROS_BRIDGE_WS_PATH/src
 
-cd $ROS_BRIDGE_WS_PATH/src
-[ ! -e ros1_bridge ] && git clone https://github.com/ros2/ros1_bridge
-cd ros1_bridge
-git fetch
-git checkout galactic
+clone_ros1_bridge() {
+  cd $ROS_BRIDGE_WS_PATH/src
+  [ ! -e ros1_bridge ] && git clone https://github.com/ros2/ros1_bridge
+  cd ros1_bridge
+  git fetch
+  git checkout galactic
+}
 
-cd $ROS_BRIDGE_WS_PATH
-source /opt/ros/galactic/setup.bash
-source $CATKIN_WS/devel/setup.bash
-colcon build --symlink-install --packages-select ros1_bridge --cmake-force-configure
+clone_message_repos() {
+  cd $ROS_BRIDGE_WS_PATH/src
+  [ ! -e mavros ] && git clone https://github.com/mavlink/mavros.git
+  cd mavros
+  git fetch
+  git checkout ros2
+
+  [ ! -e geographic_info ] && git clone https://github.com/ros-geographic-info/geographic_info.git
+  cd geographic_info
+  git fetch
+  git checkout ros2
+}
+
+build_message_repos() {
+  cd $ROS_BRIDGE_WS_PATH
+  source /opt/ros/galactic/setup.bash
+  colcon build --symlink-install --packages-select geographic_msgs
+  colcon build --symlink-install --packages-select mavros_msgs
+}
+
+build_ros1_bridge() {
+  cd $ROS_BRIDGE_WS_PATH
+  source $CATKIN_WS/devel/setup.bash
+  source $ROS_BRIDGE_WS_PATH/install/setup.bash
+  colcon build --symlink-install --packages-select ros1_bridge --cmake-force-configure
+}
+
+clone_ros1_bridge
+clone_message_repos
+build_message_repos
+build_ros1_bridge
