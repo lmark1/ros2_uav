b407f4d9d215b8fb8a847e47f98d0f878ad162c6
Lovro Markovic <lovro.markovic@fer.hr>
2023-08-31 12:53:43 +0200

    Add SSH hooks

--- run_docker.sh
+++ run_docker.sh
@@ -6,14 +6,19 @@ XAUTH=/tmp/.docker.xauth
 touch $XAUTH
 xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
 
+# Hook to the current SSH_AUTH_LOCK - since it changes
+# https://www.talkingquickly.co.uk/2021/01/tmux-ssh-agent-forwarding-vs-code/
+ln -sf $SSH_AUTH_SOCK ~/.ssh/ssh_auth_sock
+
 docker run \
-  --rm \
   -it \
   --network host \
   --privileged \
   --gpus all \
   --volume=$XSOCK:$XSOCK:rw \
   --volume=$XAUTH:$XAUTH:rw \
+  --volume ~/.ssh/ssh_auth_sock:/ssh-agent \
+  --env SSH_AUTH_SOCK=/ssh-agent \
   --env="XAUTHORITY=${XAUTH}" \
   --env DISPLAY=$DISPLAY \
   --env TERM=xterm-256color \
